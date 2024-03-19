e9317002938b01cd15a4e6943095d3ad30499f88
Mrinal Gupta <mrinalgupta04072003@gmail.com>
2024-03-20 01:33:06 +0530

    important redirective changes

--- src/main/frontend/src/Components/NavBar.js
+++ src/main/frontend/src/Components/NavBar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import {isLoggedIn, isLoggedIn_session, rank, rank_session} from "../data/constants";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import {logo} from "../Assets/images";
 
 const NavBar = () => {
     const navigate = useNavigate();
@@ -41,36 +42,36 @@ const NavBar = () => {
         <div>
             {/*{rank1 && localStorage.getItem('loggedinuserrank').match(rank1) || rank1 && sessionStorage.getItem('loggedinuserrank').match(rank1) ?*/}
                 <div className={'navbar-container'}>
-                <div className={'flex items-center justify-between mx-3'}>
-                    <h1 className={'md:text-2xl'}>ABCD</h1>
-                    {/*<HiOutlineMenuAlt3 className="text-3xl" onClick={handleToggle}/>*/}
-                    <ul className="flex items-center">
-                        {isLoggedIn || isLoggedIn_session ? (
-                            <div className={'flex'}>
-                                <li className="menuItem mx-2">
-                                    {rank1 && localStorage.getItem('loggedinuserrank').match(rank1) || rank1 && sessionStorage.getItem('loggedinuserrank').match(rank1) ?(
-                                        <Link to="/ProviderDashboard" onClick={handleMenuItemClick}>Dashboard</Link>
-                                    ) : (
-                                        <Link to="/Dashboard" onClick={handleMenuItemClick}>Dashboard</Link>
-                                    )}
-                                </li>
-                                <li className="menuItem mx-2" onClick={handleMenuItemClick}>
-                                    <Link to="/" onClick={handleLogout}>Logout</Link>
-                                </li>
-                            </div>
-                        ) : (
-                            <div className={'flex'}>
-                                <li className="menuItem mx-2">
-                                    <Link to="/SignUp" onClick={handleMenuItemClick}>Dashboard</Link>
-                                </li>
-                                <li className="menuItem mx-2" onClick={handleMenuItemClick}>
-                                    <Link to="/SignUp" onClick={handleMenuItemClick}>Sign Up/Login</Link>
-                                </li>
-                            </div>
-                        )}
-                    </ul>
+                    <div className={'flex items-center justify-between mx-3'}>
+                        <Link to="/" onClick={handleMenuItemClick}><img className={'w-40'} src={logo}/></Link>
+                        {/*<HiOutlineMenuAlt3 className="text-3xl" onClick={handleToggle}/>*/}
+                        <ul className="flex items-center">
+                            {isLoggedIn || isLoggedIn_session ? (
+                                <div className={'flex'}>
+                                    <li className="menuItem mx-2">
+                                        {rank1 && localStorage.getItem('loggedinuserrank').match(rank1) || rank1 && sessionStorage.getItem('loggedinuserrank').match(rank1) ? (
+                                            <Link to="/ProviderDashboard" onClick={handleMenuItemClick}>Dashboard</Link>
+                                        ) : (
+                                            <Link to="/Dashboard" onClick={handleMenuItemClick}>Dashboard</Link>
+                                        )}
+                                    </li>
+                                    <li className="menuItem mx-2" onClick={handleMenuItemClick}>
+                                        <Link to="/" onClick={handleLogout}>Logout</Link>
+                                    </li>
+                                </div>
+                            ) : (
+                                <div className={'flex'}>
+                                    <li className="menuItem mx-2">
+                                        <Link to="/SignUp" onClick={handleMenuItemClick}>Dashboard</Link>
+                                    </li>
+                                    <li className="menuItem mx-2" onClick={handleMenuItemClick}>
+                                        <Link to="/SignUp" onClick={handleMenuItemClick}>Sign Up/Login</Link>
+                                    </li>
+                                </div>
+                            )}
+                        </ul>
+                    </div>
                 </div>
-            </div>
             {/*: (*/}
             {/*    <div className={'navbar-container'}>*/}
             {/*        <div className={'flex items-center justify-between mx-3'}>*/}
